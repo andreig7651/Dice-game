1c251df54c37515cfeb9cb2ee4c519559600b7a2
andreig7651 <andreig7651@gmail.com>
2024-11-30 11:52:13 +0200

    dice game works

--- packages/hardhat/deploy/01_deploy_riggedRoll.ts
+++ packages/hardhat/deploy/01_deploy_riggedRoll.ts
@@ -20,11 +20,11 @@ const deployRiggedRoll: DeployFunction = async function (hre: HardhatRuntimeEnvi
   const riggedRoll: RiggedRoll = await ethers.getContract("RiggedRoll", deployer);
 
   // Please replace the text "Your Address" with your own address.
-  // try {
-  //   await riggedRoll.transferOwnership("Your Address");
-  // } catch (err) {
-  //   console.log(err);
-  // }
+  try {
+    await riggedRoll.transferOwnership("0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default deployRiggedRoll;
